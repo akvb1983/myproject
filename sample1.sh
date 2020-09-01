d6b51076b872483af874e9c23a1c21010ae485c3
akvb1983 <akvb1983@gmail.com>
2020-09-01 00:26:52 -0700

    Second Commit

--- sample1.sh
+++ sample1.sh
@@ -1,2 +1,3 @@
 #/bin/bash
 echo "This is first sample bash script created for git at : `date`"
+echo "This is sample file after editing"
---
+++ sample2.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+echo "This is another sample2 file which needs to be pushed on github"
